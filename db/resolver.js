28887e64d8ab90adc146171be8c7755e2142fc36
freilinjb <freilinjb@gmail.com>
2021-01-10 13:09:42 -0400

    Insertando Usuario en la DB

--- db/resolver.js
+++ db/resolver.js
@@ -1,44 +1,38 @@
-var cursos = [
-    {
-        titulo: "Hola mundo", tecnologia: "Hola 2"
-    },
-    {
-        titulo: "Hola mundo", tecnologia: "Hola 2"
-    },
-    {
-        titulo: "Hola mundo", tecnologia: "Hola 2"
-    },
-    {
-        titulo: "Hola mundo", tecnologia: "Hola 2"
-    },
-];
-
+const Usuario = require('../models/Usuario');
 
  /*Los resolver son funciones
  que son responsable de retornar los valores
  que existen en el esquema*/
  const resulvers = {
     Query: {
-        obtenerCursos: () => cursos,
-        obtenerTecnologia: () => cursos
+        
     },
     Mutation: {
         //primero: root, es el resultado del type padre o del type padre
         //segundo: son los argumentos que se le pasa al valor
         //tercero: es el context es el objeto que se comparte en todo el resolver
-        //cuarto: informacion de info que es relevante
-        // crearUsuario: (_,{input}, ctx) => {
-        //     console.log('_:', _);
-        //     console.log('input: ', input);
-        //     console.log('ctx: ', ctx);
-        //     console.log('Creando Usuario');
-        // }
-        crearUsuario: (_, {input}) => {
-            const { nombre, password} = input;
-            console.log(nombre);
-            console.log(password );
-            console.log('input: ', input);
-            console.log('Creando Usuario');
+        crearUsuario: async (_, {input}) => {
+            const { email, password} = input;
+            
+            const existeUsuario = await Usuario.findOne({email});
+            // console.log(existeUsuario);
+
+            //si el usuario existe
+            if(existeUsuario) {
+                throw new Error('El usuario ya esta registrado');
+            } 
+            
+            try {
+                const nuevoUsuario = new Usuario(input);
+                console.log(nuevoUsuario);
+
+                nuevoUsuario.save(); //Para guardar el registro
+                
+                //La funcion tiene que devolver un String porque asi se definio en schema
+                return "Usuario Creado Correctamente";
+            } catch (error) {
+                console.log(error);
+            }
         }
     }
 }
