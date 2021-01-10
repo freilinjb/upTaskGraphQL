c6c00db0b611cfa8814ef1c9d3e70afc1ba07379
freilinjb <freilinjb@gmail.com>
2021-01-10 19:53:04 -0400

    Probando el token del usuario autenticado en el Proyecto

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-const {ApolloServer} = require('apollo-server');
+const { ApolloServer } = require("apollo-server");
+const jwt = require('jsonwebtoken');
+require('dotenv').config('variables.env');
 
-const typeDefs = require('./db/schema');
-const resolvers = require('./db/resolver');
+const typeDefs = require("./db/schema");
+const resolvers = require("./db/resolver");
 
-const conectarDB = require('./config/db');
+const conectarDB = require("./config/db");
+
 
 //Conectar a la DB
 conectarDB();
 
-const server = new ApolloServer({typeDefs, resolvers});
+const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers,
+    /*context es una funcion que estara presente
+    en todos los resolver y funciona y igual que el req y el res de nodejs
+    */
+    context: ({req}) => {
+        const token = req.headers['autorization'] || '';
+        // console.log(req.headers);
+        // console.log('token: ', token);
+        if(token) {
+            try {
+                const usuario = jwt.varify(token, process.env.SECRETA);
+                console.log('usuario: ', usuario);
+
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+ });
 
 // Listen retorna una promesa
-server.listen().then(({url}) => {
-    console.log(`Servidor listo en la URL ${url}`)
-})
+server.listen().then(({ url }) => {
+  console.log(`Servidor listo en la URL ${url}`);
+});
